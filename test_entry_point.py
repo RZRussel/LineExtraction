65aa5b46899b886f202e76cbd628de86b1549f39
Xomak <mail@xomak.net>
2017-10-23 21:35:26 +0300

    [New] Add test_entry_point

---
+++ test_entry_point.py
@@ -0,0 +1,33 @@
+from sympy import Point2D
+
+from base import Area
+from finders import RDPSegmentsFinder, PolylinesFinder
+from readers import XYFileAreaReader
+from visualisers import MatplotlibVisualiser
+
+# Test entry point for development purposes
+
+
+def get_square_area():
+    top = [Point2D(x, 0) for x in range(0, 5)]
+    right = [Point2D(4, y) for y in range(1, 5)]
+    bottom = [Point2D(x, 4) for x in range(3, 0, -1)]
+    left = [Point2D(0, y) for y in range(4, 0, -1)]
+    square = top + right + bottom + left
+
+    area = Area()
+    for point in square:
+        area.add_object(Point2D, point)
+
+    return area
+
+area = XYFileAreaReader.get_area("example/7.xy")
+
+polylines_finder = PolylinesFinder(epsilon=150)
+area = polylines_finder.find(area)
+
+rdp_finder = RDPSegmentsFinder(epsilon=20)
+area = rdp_finder.find(area)
+
+t = MatplotlibVisualiser()
+t.draw(area)
