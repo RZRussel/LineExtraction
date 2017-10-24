1ac9edaa49020605d08fa6a7cef65a6453a25b91
Russel <emkil.russel@gmail.com>
2017-10-24 16:11:40 +0300

    [New] LinearRegressionCoordinator added for merging entities

--- tests/test_line_regression_segments_finder.py
+++ tests/test_line_regression_segments_finder.py
@@ -1,7 +1,7 @@
 import unittest
 import math
 from sympy import Point2D
-from lineregression import LinearRegressionEntity, LineRegressionSegmentsFinder
+from lineregression import LinearRegressionEntity, LinearRegressionCoordinator, LineRegressionSegmentsFinder
 
 
 class TestLineRegressionSegmentsFinder(unittest.TestCase):
@@ -40,13 +40,13 @@ class TestLineRegressionSegmentsFinder(unittest.TestCase):
         points = []
 
         for i in range(0, 20):
-            points.append(Point2D(1 + i*0.1, 0.0))
+            points.append(Point2D(i*0.1, 0.0))
         for i in range(0, 20):
-            points.append(Point2D(1 + 2.0, i*0.1))
+            points.append(Point2D(2.0 + i*0.1, i*0.1))
         for i in range(0, 20):
-            points.append(Point2D(1 + 2 - i*0.1, 2.0))
+            points.append(Point2D(4.0 - i*0.1, 2.0))
         for i in range(0, 20):
-            points.append(Point2D(1 + 0.0, 2 - i * 0.1))
+            points.append(Point2D(2.0 - i*0.1, 2.0 - i * 0.1))
 
         finder = LineRegressionSegmentsFinder(7, 3, 0.5)
         entities = finder._perform_segmentation(points)
@@ -99,3 +99,77 @@ class TestLinearRegression(unittest.TestCase):
         weighted_entity = LinearRegressionEntity.weighted_mean_entity([entity])
         self.assertAlmostEquals(first=entity.slope, second=weighted_entity.slope, delta=1e-6)
         self.assertAlmostEquals(first=entity.offset, second=weighted_entity.offset, delta=1e-6)
+
+class TestLinearRegressionCoordination(unittest.TestCase):
+
+    def test_init(self):
+        points = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity = LinearRegressionEntity(points)
+        coordinator = LinearRegressionCoordinator(entity, 10)
+
+        self.assertEqual(first=entity, second=coordinator.entity)
+        self.assertEqual(first=coordinator.start_index, second=10)
+
+    def test_can_merge_false(self):
+        points1 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity1 = LinearRegressionEntity(points1)
+        coordinator1 = LinearRegressionCoordinator(entity1, 2)
+
+        points2 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity2 = LinearRegressionEntity(points2)
+        coordinator2 = LinearRegressionCoordinator(entity2, 8)
+
+        self.assertFalse(coordinator2.can_merge(coordinator1))
+
+    def test_can_merge_true(self):
+        points1 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity1 = LinearRegressionEntity(points1)
+        coordinator1 = LinearRegressionCoordinator(entity1, 2)
+
+        points2 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity2 = LinearRegressionEntity(points2)
+        coordinator2 = LinearRegressionCoordinator(entity2, 5)
+
+        self.assertTrue(coordinator2.can_merge(coordinator1))
+
+    def test_merge_intersected(self):
+        points1 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity1 = LinearRegressionEntity(points1)
+        coordinator1 = LinearRegressionCoordinator(entity1, 2)
+
+        points2 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity2 = LinearRegressionEntity(points2)
+        coordinator2 = LinearRegressionCoordinator(entity2, 6)
+
+        coordinator = coordinator2.merge(coordinator1)
+
+        self.assertEqual(first=coordinator.start_index, second=2)
+        self.assertEqual(first=len(coordinator.entity.points), second=9)
+
+    def test_merge_separated(self):
+        points1 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity1 = LinearRegressionEntity(points1)
+        coordinator1 = LinearRegressionCoordinator(entity1, 2)
+
+        points2 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity2 = LinearRegressionEntity(points2)
+        coordinator2 = LinearRegressionCoordinator(entity2, 7)
+
+        coordinator = coordinator2.merge(coordinator1)
+
+        self.assertEqual(first=coordinator.start_index, second=2)
+        self.assertEqual(first=len(coordinator.entity.points), second=10)
+
+    def test_merge_same(self):
+        points1 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity1 = LinearRegressionEntity(points1)
+        coordinator1 = LinearRegressionCoordinator(entity1, 2)
+
+        points2 = [Point2D(6, 5), Point2D(7, 4), Point2D(8, 7), Point2D(5, 6), Point2D(5, 4)]
+        entity2 = LinearRegressionEntity(points2)
+        coordinator2 = LinearRegressionCoordinator(entity2, 2)
+
+        coordinator = coordinator2.merge(coordinator1)
+
+        self.assertEqual(first=coordinator.start_index, second=2)
+        self.assertEqual(first=len(coordinator.entity.points), second=5)
